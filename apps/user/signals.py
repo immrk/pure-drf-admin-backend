fb02237ef4f361163e2cf9d6fd618d52e311e9cf
yajian.ke <keyajian@gmail.com>
2024-10-29 12:57:33 +0800

    [add]增加权限(菜单)数据更新时，redis缓存刷新机制

--- apps/user/signals.py
+++ apps/user/signals.py
@@ -1,17 +1,9 @@
 # users/signals.py
 
-from django.db.models.signals import pre_delete
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from .models import User
 from django.conf import settings
 from django.core.cache import cache
-
-
-@receiver(pre_delete, sender=User)
-def delete_user_role_relationship(sender, instance, **kwargs):
-    # 清除与被删除用户相关的所有角色关联
-    instance.role.clear()
 
 @receiver(user_logged_in)
 def user_logged_in_handler(sender, request, user, **kwargs):
