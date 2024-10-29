0eeb0bef3b1f4e7bb18ed75d45f2f8dc6e310c44
yajian.ke <keyajian@gmail.com>
2024-10-29 12:57:33 +0800

    [add]增加权限(菜单)数据更新时，redis缓存刷新机制

--- apps/system/models.py
+++ apps/system/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from utils.models import UuidModel, BaseModel
-from rest_framework.utils import encoders
-from django.core.exceptions import ValidationError
-import json
 
 
 class Role(UuidModel, BaseModel):
